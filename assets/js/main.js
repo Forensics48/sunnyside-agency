07c959164dc98042d27368473e68466e06b53373
Antoan Atanasov <atanasov@stud.hs-heilbronn.de>
2022-06-11 21:50:17 +0200

    Finish project

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,27 @@ if (navToggle) {
         }
     });
 }
+
+document.addEventListener('scroll', function(e) {
+    var list = $("#header").collision(".white-content");
+    if (list.length > 0) {
+        $("#header .nav__link").addClass('on-white');
+        $(".nav__logo-image").addClass('on-white');
+        /*
+        $("#header .nav__link").css("color", "var(--primary-color-dark-cyan)");
+        $(".nav__logo-image").css("filter", "invert(100%)");
+        $("#header .nav__contact").css("background-color", "var(--primary-color-dark-blue)");
+        $("#header .nav__contact").css("color", "var(--white-color)");
+        */
+    } else {
+        /*
+        $("#header .nav__link").css("color", "var(--white-color)");
+        $("#header .nav__contact").css("background-color", "var(--white-color)");
+        $("#header .nav__contact").css("color", "var(--primary-color-dark-cyan)")
+        $(".nav__logo-image").css("filter", "none");
+        */
+        $("#header .nav__link").removeClass('on-white');
+        $(".nav__logo-image").removeClass('on-white');
+    }
+    console.log(list.length);
+});
